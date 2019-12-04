d6cae7b1d22fd43865825ab2c57940c5dfa27cf3
Gaute Meek Olsen <gaute.meek@gmail.com>
2019-12-04 22:49:43 +0100

    finished day 4

--- Day4/src/Secure.kt
+++ Day4/src/Secure.kt
@@ -1,28 +1,24 @@
 
 fun main(){
-    val chars = 123.toString()
     var count = 0
     for(i in 382345..843167){
-        if(isPassword(i)){
+        if(isPassword(i, true)){
             count++
         }
     }
     println(count)
 }
 
-fun isPassword(number: Int): Boolean{
+fun isPassword(number: Int, onlyDouble: Boolean): Boolean{
     val chars = number.toString()
-    var double = false
     for(i in 1 until 6){
-        if(chars[i] == chars[i-1] && (i < 5 && chars[i+1] != chars[i])){
-            double = true
-        }
         if(chars[i].toInt() < chars[i-1].toInt()){
             return false
         }
     }
-    if(double){
-        println(number)
-    }
-    return double
+    return if(onlyDouble) hasDouble(chars) else hasRepeating(chars)
 }
+
+fun hasDouble(numbers: String) = Regex("""([0-9])\1+""").findAll(numbers).any { it.value.length == 2 }
+
+fun hasRepeating(numbers: String) = !Regex("""([0-9])\1+""").findAll(numbers).none()
--- Day4/test/SecureKtTest.kt
+++ Day4/test/SecureKtTest.kt
@@ -7,28 +7,35 @@ internal class SecureKtTest {
     @Test
     fun isPassword() {
         val input = 111111
-        val res = isPassword(input)
-        assertEquals(false, res) //false for part 2 and true for part 1
+        val res = isPassword(input, false)
+        assertEquals(true, res)
     }
 
     @Test
     fun `is not pw because of decrease`() {
         val input = 223450
-        val res = isPassword(input)
+        val res = isPassword(input, false)
         assertEquals(false, res)
     }
 
     @Test
     fun `is not pw because of no double`() {
         val input = 123789
-        val res = isPassword(input)
+        val res = isPassword(input, false)
         assertEquals(false, res)
     }
 
     @Test
     fun `is not pw because of tripple`() {
         val input = 123444
-        val res = isPassword(input)
+        val res = isPassword(input, true)
         assertEquals(false, res)
+    }
+
+    @Test
+    fun `is pw with double`() {
+        val input = 123449
+        val res = isPassword(input, true)
+        assertEquals(true, res)
     }
 }
